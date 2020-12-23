b825bc6bb514b89f1ebe092bd577162231f47cd5
KathTheDragon <kaththedragon@gmail.com>
2020-12-23 11:40:27 +0000

    Fix tests

--- tests/test_words.py
+++ tests/test_words.py
@@ -4,10 +4,10 @@ from SCE.src import words
 
 def test_word_iter_returns_phones():
     phones = ['a', 'b', 'c', 'd', 'e']
-    assert list(Word(phones) == phones
+    assert list(words.Word(phones) == phones
 
 def test_word_parse_adds_surrounding_hashes():
-    assert Word.parse('abc').phones == ['#', 'a', 'b', 'c', '#']
+    assert words.Word.parse('abc').phones == ['#', 'a', 'b', 'c', '#']
 
 def test_word_unparse_strips_surrounding_spaces():
-    assert Word(['#', 'a', 'b', 'c', '#']).unparse() == 'abc'
+    assert words.Word(['#', 'a', 'b', 'c', '#']).unparse() == 'abc'
