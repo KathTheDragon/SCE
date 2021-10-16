c56fceabd062e6290af0b04875d412379d6241e1
KathTheDragon <kaththedragon@gmail.com>
2021-10-16 12:28:45 +0100

    Import all names from words

--- tests/test_words.py
+++ tests/test_words.py
@@ -1,82 +1,82 @@
 import pytest
 
-from SCE.src import words
+from SCE.src.words import *
 
 ## Word ##
 
 def test_Word_iter_returns_phones():
     phones = ['a', 'b', 'c', 'd', 'e']
-    assert list(words.Word(phones)) == phones
+    assert list(Word(phones)) == phones
 
 def test_Word_parse_adds_surrounding_hashes():
-    assert words.Word.parse('abc').phones == ['#', 'a', 'b', 'c', '#']
+    assert Word.parse('abc').phones == ['#', 'a', 'b', 'c', '#']
 
 def test_Word_str_strips_surrounding_spaces():
-    assert str(words.Word(['#', 'a', 'b', 'c', '#'])) == 'abc'
+    assert str(Word(['#', 'a', 'b', 'c', '#'])) == 'abc'
 
 ## parse ##
 
 def test_parse_rejects_characters_not_in_graphemes():
-    with pytest.raises(words.InvalidCharacter,
+    with pytest.raises(InvalidCharacter,
     match=r"^Encountered character 'a' not in graphemes \['b', 'c', 'd'\] while parsing string 'a'$"):
-        words.parse('a', ('b', 'c', 'd'))
+        parse('a', ('b', 'c', 'd'))
 
 def test_parse_prefers_longest_grapheme():
-    assert words.parse('abc', ('a', 'b', 'c', 'ab', 'bc', 'abc')) == ['abc']
+    assert parse('abc', ('a', 'b', 'c', 'ab', 'bc', 'abc')) == ['abc']
 
 def test_parse_override_with_separator():
-    assert words.parse('abc', ('a', 'b', 'c', 'ab', 'bc', 'abc'), '.') == ['abc']
-    assert words.parse('a.bc', ('a', 'b', 'c', 'ab', 'bc', 'abc'), '.') == ['a', 'bc']
-    assert words.parse('ab.c', ('a', 'b', 'c', 'ab', 'bc', 'abc'), '.') == ['ab', 'c']
-    assert words.parse('a.b.c', ('a', 'b', 'c', 'ab', 'bc', 'abc'), '.') == ['a', 'b', 'c']
+    assert parse('abc', ('a', 'b', 'c', 'ab', 'bc', 'abc'), '.') == ['abc']
+    assert parse('a.bc', ('a', 'b', 'c', 'ab', 'bc', 'abc'), '.') == ['a', 'bc']
+    assert parse('ab.c', ('a', 'b', 'c', 'ab', 'bc', 'abc'), '.') == ['ab', 'c']
+    assert parse('a.b.c', ('a', 'b', 'c', 'ab', 'bc', 'abc'), '.') == ['a', 'b', 'c']
 
 def test_parse_wildcard_graphemes_match_anything():
-    assert words.parse('abc', ('*',)) == ['a', 'b', 'c']
-    assert words.parse('abbbcb', ('*b',)) == ['ab', 'bb', 'cb']
+    assert parse('abc', ('*',)) == ['a', 'b', 'c']
+    assert parse('abbbcb', ('*b',)) == ['ab', 'bb', 'cb']
 
 ## unparse ##
 
 def test_unparse_joins_word_when_only_monographs():
-    assert words.unparse(('a', 'b', 'c'), ('a', 'b', 'c')) == 'abc'
+    assert unparse(('a', 'b', 'c'), ('a', 'b', 'c')) == 'abc'
 
 # Should probably change
 def test_unparse_allows_characters_not_in_graphemes():
-    assert words.unparse(('a', 'b', 'c'), ('ph', 'th', 'kh')) == 'abc'
+    assert unparse(('a', 'b', 'c'), ('ph', 'th', 'kh')) == 'abc'
 
 def test_unparse_inserts_separator_in_sequences_matching_multiple_graphemes():
-    assert words.unparse(('a', 'b', 'c'), ('a', 'b', 'c', 'ab', 'bc', 'abc'), '.') == 'a.b.c'
-    assert words.unparse(('ab', 'c'), ('a', 'b', 'c', 'ab', 'bc', 'abc'), '.') == 'ab.c'
-    assert words.unparse(('a', 'bc'), ('a', 'b', 'c', 'ab', 'bc', 'abc'), '.') == 'a.bc'
-    assert words.unparse(('abc',), ('a', 'b', 'c', 'ab', 'bc', 'abc'), '.') == 'abc'
+    assert unparse(('a', 'b', 'c'), ('a', 'b', 'c', 'ab', 'bc', 'abc'), '.') == 'a.b.c'
+    assert unparse(('ab', 'c'), ('a', 'b', 'c', 'ab', 'bc', 'abc'), '.') == 'ab.c'
+    assert unparse(('a', 'bc'), ('a', 'b', 'c', 'ab', 'bc', 'abc'), '.') == 'a.bc'
+    assert unparse(('abc',), ('a', 'b', 'c', 'ab', 'bc', 'abc'), '.') == 'abc'
 
 ## startswith ##
 
 def test_startswith_returns_false_when_prefix_longer_than_string():
-    assert not words.startswith('ab', 'abc')
-    assert not words.startswith('ab', 'abc', strict=True)
+    assert not startswith('ab', 'abc')
+    assert not startswith('ab', 'abc', strict=True)
 
 def test_startswith_returns_false_when_prefix_equals_string_and_strict_is_true():
-    assert not words.startswith('ab', 'ab', strict=True)
+    assert not startswith('ab', 'ab', strict=True)
 
 def test_startswith_returns_false_when_prefix_is_not_a_prefix_of_string():
-    assert not words.startswith('ab', 'b')
-    assert not words.startswith('ab', 'b', strict=True)
+    assert not startswith('ab', 'b')
+    assert not startswith('ab', 'b', strict=True)
 
 def test_startswith_returns_true_when_prefix_is_a_prefix_of_string():
-    assert words.startswith('ab', 'a')
-    assert words.startswith('ab', 'ab')
-    assert words.startswith('ab', 'a', strict=True)
+    assert startswith('ab', 'a')
+    assert startswith('ab', 'ab')
+    assert startswith('ab', 'a', strict=True)
 
 def test_startswith_allows_star_to_match_any_character():
-    assert words.startswith('ab', '*')
-    assert words.startswith('ab', 'a*')
-    assert words.startswith('ab', '*b')
-    assert words.startswith('ab', '**')
-    assert not words.startswith('ab', '**', strict=True)
+    assert startswith('ab', '*')
+    assert startswith('ab', 'a*')
+    assert startswith('ab', '*b')
+    assert startswith('ab', '**')
+    assert not startswith('ab', '**', strict=True)
     #
-    assert words.startswith('*b', 'a')
-    assert words.startswith('*b', 'a*')
-    assert words.startswith('*b', 'ab')
-    assert not words.startswith('*b', 'ab', strict=True)
+    assert startswith('*b', 'a')
+    assert startswith('*b', 'a*')
+    assert startswith('*b', 'ab')
+    assert not startswith('*b', 'ab', strict=True)
 
 ## parseWords ##
